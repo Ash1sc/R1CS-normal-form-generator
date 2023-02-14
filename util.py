3c7964cf630163bb6806ef6b501140d6985e44d2
un-defeated <121156719+Ash1sc@users.noreply.github.com>
2023-02-14 11:41:10 +0800

    修改rnode设计,添加matrix预处理, 完成建树最简单情况

--- util.py
+++ util.py
@@ -26,4 +26,21 @@ def make_matrix():
         for j, num in enumerate(fields):
             matrices[matrix_id][i - prev_line_index - 1][j] = int(num)
 
+
+    #   确保每一条限制中c的field中一定有一个是1
+    for k in range(len(matrices[0])):
+
+        # 找到限制组c中大于等于1的最小的数
+        target = 0
+
+        for j in range(len(matrices[0][0])):
+            if target == 0 or (0 < matrices[2][k][j] < target):
+                # print("c[%d][%d]: %d, target %d" % (k, j, matrices[2][k][j], target))
+                target = matrices[2][k][j]
+
+        # 限制组a与c同时放缩至1/target
+        for j in range(len(matrices[0][0])):
+            matrices[0][k][j] = matrices[0][k][j] / target
+            matrices[2][k][j] = matrices[2][k][j] / target
+
     return matrices[0], matrices[1], matrices[2]
